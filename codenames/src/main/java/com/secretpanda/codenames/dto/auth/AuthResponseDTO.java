a3885edf75867833553df30cc5f36a51fa5511d3
rociocholiz <r.choliz16@gmail.com>
2026-03-11 00:57:54 +0100

    primeros DTOs

--- codenames/src/main/java/com/secretpanda/codenames/dto/auth/AuthResponseDTO.java
+++ codenames/src/main/java/com/secretpanda/codenames/dto/auth/AuthResponseDTO.java
@@ -1,5 +1,80 @@
 package com.secretpanda.codenames.dto.auth;
 
+import com.secretpanda.codenames.model.Jugador;
+
 public class AuthResponseDTO {
     
+    // El token JWT generado por JwtService
+    private String token;
+    
+    // Los datos del perfil de usuario para la pantalla de inicio
+    private PerfilBasicoDTO perfil;
+
+    // Constructor vacío
+    public AuthResponseDTO() {
+    }
+
+    // Constructor con parámetros
+    public AuthResponseDTO(String token, Jugador jugador) {
+        this.token = token;
+        this.perfil = new PerfilBasicoDTO(jugador);
+    }
+
+    // Getters y Setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public PerfilBasicoDTO getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(PerfilBasicoDTO perfil) {
+        this.perfil = perfil;
+    }
+
+    // CLASE ANIDADA PARA EL PERFIL DEL USUARIO
+    // Solo enviamos lo que hace falta al iniciar sesión
+    public static class PerfilBasicoDTO {
+        //Parámetros para el perfil
+        private String idGoogle;
+        private String tag;
+        private String fotoPerfil;
+        private Integer balas;
+        private Integer victorias;
+
+        // Constructor que recibe el jugador para manejar los datos
+        public PerfilBasicoDTO(Jugador jugador) {
+            this.idGoogle = jugador.getIdGoogle();
+            this.tag = jugador.getTag();
+            this.fotoPerfil = jugador.getFotoPerfil();
+            this.balas = jugador.getBalas();
+            this.victorias = jugador.getVictorias();
+        }
+
+        // Getters
+        public String getIdGoogle() { 
+            return idGoogle; 
+        }
+
+        public String getTag() { 
+            return tag; 
+        }
+
+        public String getFotoPerfil() { 
+            return fotoPerfil; 
+        }
+
+        public Integer getBalas() { 
+            return balas; 
+        }
+        
+        public Integer getVictorias() { 
+            return victorias; 
+        }
+    }
 }
--- codenames/src/main/java/com/secretpanda/codenames/dto/auth/LoginRequestDTO.java
+++ codenames/src/main/java/com/secretpanda/codenames/dto/auth/LoginRequestDTO.java
@@ -2,4 +2,31 @@ package com.secretpanda.codenames.dto.auth;
 
 public class LoginRequestDTO {
     
+    // Es el "token" que devuelve el SDK de Google al hacer el login 
+    // Lo usaremos para verificar la identidad del usuario
+    private String idToken;
+    
+    // Es el "nickname" que el usuario va a usar en la aplicación
+    private String tag;
+
+    // Contructor vacío
+    public LoginRequestDTO() {
+    }
+
+    // Getters y Setters
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
